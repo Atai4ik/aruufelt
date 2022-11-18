5b9b678b4d7049a23ebf6d8a25185ac9c1c22190
Atai4ik <atai.zh@gmail.com>
2022-11-18 10:57:22 +0600

    - add category in models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Genre(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    parent = TreeForeignKey('self', related_name='children', on_delete=models.SET_NULL, null=True)
 
 
 class Tag(models.Model):
@@ -8,10 +20,11 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, related_name='posts', on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='articles/')
     text = models.TextField()
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='articles/')
+    category = models.ForeignKey(Category, related_name='post', on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag, related_name='post')
     created_at = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0)
